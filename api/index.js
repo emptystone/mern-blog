7090c0694a40b8badbc42fcdb10ea9e2a362c74f
emptystone <emptystone123@gmail.com>
2024-10-11 11:10:04 +0700

    create signup api route

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-// import userRoutes from './routes/user.route.js';
-// import authRoutes from './routes/auth.route.js';
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 // import postRoutes from './routes/post.route.js';
 // import commentRoutes from './routes/comment.route.js';
 // import cookieParser from 'cookie-parser';
@@ -19,28 +19,23 @@ mongoose
     console.log(err);
   });
 
-// mongoose
-//   .connect(process.env.MONGO)
-//   .then(() => {
-//     console.log('MongoDb is connected');
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
-
 // const __dirname = path.resolve();
 
 const app = express();
 
-// app.use(express.json());
+app.use(express.json());
 // app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000!');
 });
 
-// app.use('/api/user', userRoutes);
-// app.use('/api/auth', authRoutes);
+app.get('/test', (req, res) => {
+  res.json({ message: 'Hello, this is the API route!' });
+});
+
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
 // app.use('/api/post', postRoutes);
 // app.use('/api/comment', commentRoutes);
 
---
+++ api/routes/user.route.js
@@ -0,0 +1,25 @@
+import express from 'express';
+// import {
+//   deleteUser,
+//   getUser,
+//   getUsers,
+//   signout,
+//   test,
+//   updateUser,
+// } from '../controllers/user.controller.js';
+// import { verifyToken } from '../utils/verifyUser.js';
+
+const router = express.Router();
+
+router.get('/test', (req, res) => {
+  res.send('Hello, this is the API route!');
+});
+
+// router.get('/test', test);
+// router.put('/update/:userId', verifyToken, updateUser);
+// router.delete('/delete/:userId', verifyToken, deleteUser);
+// router.post('/signout', signout);
+// router.get('/getusers', verifyToken, getUsers);
+// router.get('/:userId', getUser);
+
+export default router;
