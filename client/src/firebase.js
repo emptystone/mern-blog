66b7df5e87c35751e3daecb573daebecb40ec80a
emptystone <emptystone123@gmail.com>
2024-10-26 06:45:14 +0700

    add redux and persist

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,21 +1,21 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from 'firebase/app';
+// import { getAnalytics } from 'firebase/analytics';
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE,
-  authDomain: 'mern-blog-b3a1e.firebaseapp.com',
-  projectId: 'mern-blog-b3a1e',
-  storageBucket: 'mern-blog-b3a1e.appspot.com',
-  messagingSenderId: '780351696828',
-  appId: '1:780351696828:web:0eca53f6c80050cbe745b9',
-  measurementId: 'G-LV81GE5J4J',
-};
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE,
+//   authDomain: 'mern-blog-b3a1e.firebaseapp.com',
+//   projectId: 'mern-blog-b3a1e',
+//   storageBucket: 'mern-blog-b3a1e.appspot.com',
+//   messagingSenderId: '780351696828',
+//   appId: '1:780351696828:web:0eca53f6c80050cbe745b9',
+//   measurementId: 'G-LV81GE5J4J',
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -7,12 +7,20 @@ import {
 } from 'flowbite-react';
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from '../redux/user/userSlice';
 // import OAuth from '../components/OAuth';
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const { loading, error: errorMessage } = useSelector(
+    (state) => state.user
+  );
+  const dispatch = useDispatch();
   const navigate = useNavigate();
   const handleChange = (e) => {
     setFormData({
@@ -23,11 +31,12 @@ export default function SignIn() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (!formData.email || !formData.password) {
-      return setErrorMessage('Please fill out all fields.');
+      return dispatch(
+        signInFailure('Please fill all the fields')
+      );
     }
     try {
-      setLoading(true);
-      setErrorMessage(null);
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
@@ -35,16 +44,15 @@ export default function SignIn() {
       });
       const data = await res.json();
       if (data.success === false) {
-        setLoading(false);
-        return setErrorMessage(data.message);
+        dispatch(signInFailure(data.message));
       }
-      setLoading(false);
+
       if (res.ok) {
+        dispatch(signInSuccess(data));
         navigate('/');
       }
     } catch (error) {
-      setErrorMessage(error.message);
-      setLoading(false);
+      dispatch(signInFailure(error.message));
     }
   };
   return (
@@ -110,7 +118,7 @@ export default function SignIn() {
           <div className="flex gap-2 text-sm mt-5">
             <span>Dont Have an account?</span>
             <Link to="/sign-up" className="text-blue-500">
-              Sign up
+              Sign Up
             </Link>
           </div>
           {errorMessage && (
